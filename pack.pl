47302244d9133da8ed0548682680f14d9fd543a6
root <logicmoo@gmail.com>
2017-03-16 20:55:26 -0700

    v1.1.114

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(subclause_expansion).
 title('More detailed versions of term/goal expansion hooks').
 
-version('1.1.113').
+version('1.1.114').
 download('https://github.com/TeamSPoon/subclause_expansion/releases/*.zip').
 
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
